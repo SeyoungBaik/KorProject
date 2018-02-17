cb19ed584777618ec737b730262074722063b6e5
Seyoung Baik <hielelshadday@gmail.com>
2018-02-17 05:12:52 +0000

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  rolify
+  
+  include Authority::UserAbilities
+  # Include default devise modules. Others available are:
+  #  :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+         
+         has_many :posts, dependent: :destroy
+         
+end
